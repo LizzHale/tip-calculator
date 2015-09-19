29e5fac1925dad66bb2069c3fab1dd3cb5f3fdd4
Elizabeth Hale <lizzhale@mac.com>
2015-09-19 14:15:36 -0700

    Tells the user who ordered what, how much it cost, and what the total was

--- meal_calculator.js
+++ meal_calculator.js
@@ -1,23 +1,16 @@
 // Create diner object which represents a single diner
 // have the ability to add dishes to diner's meal
-function Diner() {
+function Diner(name) {
     // meal consists of a array of dishes
-    this.name = null,
+    this.name = name,
     this.meal = [],
     this.tax = 0.1,
     this.tip = 0.2;
-    this.mealCost = function (){
-        var cost = 0;
-        this.meal.forEach(function (dish, index){
-            console.log("This is the cost right now " + cost);
-            cost += dish.itemCost;
-        });
-        cost = (cost * this.tip) + (cost * this.tip) + cost;
-        return cost;
-    };
+    this.mealCost = 0;
 };
 
-function Dish(cost) {
+function Dish(name, cost) {
+    this.name = name;
     this.itemCost = cost;
 };
 
@@ -26,29 +19,32 @@ function Dish(cost) {
 var bill = function(diners) {
     var total = 0;
     diners.forEach(function (diner, index){
-        console.log("The total for " + diner.name + " is: " + diner.mealCost());
-        total += diner.mealCost();
+        var cost = 0;
+        diner.meal.forEach(function (dish, index){
+            console.log(diner.name + " has ordered " + dish.name + " which cost: $" + dish.itemCost.toFixed(2))
+            cost += dish.itemCost;
+        })
+        cost = (cost * diner.tax) + (cost * diner.tip) + cost;
+        console.log(diner.name + "'s total with tax and tip is: $" + cost.toFixed(2));
+        total += cost;
     });
-    console.log("The total for the table is " + total);
-    return total;
+    console.log("The total for the table is: $" + total.toFixed(2));
 }
 
 // A few dishes
-var poutine = new Dish(10);
-var breadSticks = new Dish(3);
-var steakFrites = new Dish(15);
-var hamburger = new Dish(8);
-var meatballs = new Dish(11);
-var brusselSprouts = new Dish(7);
-var sideSalad = new Dish(5);
-var soda = new Dish(2);
-var iceCream = new Dish(4.5);
+var poutine = new Dish("poutine", 10);
+var breadSticks = new Dish("bread sticks", 3);
+var steakFrites = new Dish("steak frites", 15);
+var hamburger = new Dish("hamburger", 8);
+var meatballs = new Dish("meatballs", 11);
+var brusselSprouts = new Dish("brussel sprouts", 7);
+var sideSalad = new Dish("side salad", 5);
+var soda = new Dish("soda", 2);
+var iceCream = new Dish("ice cream", 4.5);
 
-var diana = new Diner();
-var mike = new Diner();
+var diana = new Diner('Mike');
+var mike = new Diner('Diana');
 
-mike.name = 'Mike';
-diana.name = 'Diana';
 mike.meal = [poutine, sideSalad, soda];
 diana.meal = [steakFrites, brusselSprouts, iceCream];
 
